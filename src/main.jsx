f2030eb64d96c9e1fb383de34afa2be6326895a8
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-09-29 22:25:27 +0800

    add: react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter, Route, Routes } from "react-router-dom";
 import App from './App'
 import './index.css'
 
+import ToDoList from "./TodoList";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={ <App /> } />
+        <Route path="/todolist" element={ <ToDoList /> } />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 )
